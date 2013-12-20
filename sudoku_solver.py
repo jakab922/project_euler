7eaddbdceeb59b8e08d3fc5526300fc965890bf0
Daniel Papp <jakab922@gmail.com>
2013-12-20 16:07:18 +0000

    Added some more stuff to the sudoku solver

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,20 +1,62 @@
-def _reverse_modified(modified, value, possibles, pcounts):
+from itertools import product
+
+class Possible(object):
+    def __init__(self):
+        self.values = [True for _ in xrange(10)]
+        self.good_count = 9
+
+    @property
+    def count(self):
+        return self.good_count
+
+    def use(self, index):
+        self.values[index] = False
+        self.good_count -= 1
+
+    def unuse(self, index):
+        self.values[index] = False
+        self.good_count += 1
+
+    def __getitem__(self, index):
+        return self.values[index]
+
+
+class TableSteps(object):
+    def __init__(self, step_list):
+        self.step_list = step_list
+        self.sll = len(step_list)
+        self.step_index = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.step_index < self.sll:
+            self.step_index += 1
+            return self.step_list[self.step_index - 1]
+        else:
+            raise StopIteration
+
+    @property
+    def rest(self):
+        return self.step_list[self.step_index:]
+
+
+def _reverse_modified(modified, value, possibles):
     for i, j in modified:
-        possibles[i][j][value] = True
-        pcounts[i][j] += 1
+        possibles[i][j].unuse(value)
 
 
-def _decrement_in_range(ranges, position, value, possibles, pcounts, modified):
+def _decrement_in_range(ranges, position, value, possibles, modified):
     i_low, i_high, j_low, j_high = ranges
     ci, cj = position
     for i in xrange(i_low, i_high):
         for j in xrange(j_low, j_high):
             if (i, j) not in modified:
-                if (ci != i or cj != j) and pcounts[i][j] == 1:
+                if (ci != i or cj != j) and possibles[i][j].count == 1:
                     return False
-                else:
-                    pcounts[i][j] -= 1
-                    possibles[i][j][value] = False
+                elif posibles[i][j][value]:
+                    possibles[i][j].use(value)
                     modified.add((i, j))
 
     return True
@@ -22,9 +64,6 @@ def _decrement_in_range(ranges, position, value, possibles, pcounts, modified):
 
 def make_step(table, position, value, possibles, pcounts):
     i, j = position
-    if not possibles[x][y][value]:  # Probably we should check against this outside
-        return False
-    row_needs, col_needs, box_needs = needs
     bn = 3 * (i / 3) + j / 3
 
     modified = set()
@@ -35,78 +74,70 @@ def make_step(table, position, value, possibles, pcounts):
     ]
 
     for crange in ranges:
-        valid = _decrement_in_range(crange, position, value, possibles,
-                                    pcounts, modified)
+        valid = _decrement_in_range(crange, position, value,
+                                    possibles, modified)
         if not valid:
-            _reverse_modified(modified, value, possibles, pcounts)
+            _reverse_modified(modified, value, possibles)
             return (False, None)
 
     table[i][j] = value
     return (True, modified)
 
 
-def reverse_step(table, position, value, possibles, pcounts, modified):
-    ci, cj = position
-    for i, j in modified:
-        possibles[i][j][value] = True
-        pcounts[i][j] += 1
-
-    table[ci][cj] = 0
+def _freeze_table(table):
+    return tuple([tuple(row) for row in table])
 
 
 def solve(sudoku_table, all_solutions=False):
-    pcounts = [[9 for __ in xrange(9)] for _ in xrange(9)]
-    possibles = [[[True for _ in xrange(9)] for _j in xrange(9)]
-                 for _i in xrange(9)]
-    possible_steps = set()
+    r3, r9, r10 = range(3), range(9), range(10)
 
-    boxes = [[False for __ in xrange(10)] for _ in xrange(9)]
-    rows = [[False for __ in xrange(10)] for _ in xrange(9)]
-    cols = [[False for __ in xrange(10)] for _ in xrange(9)]
-
-    was = set()
+    possibles = [[Possible() for __ in r9] for _ in r9]
+    boxes = [[False for __ in r10] for _ in r9]
+    rows = [[False for __ in r10] for _ in r9]
+    cols = [[False for __ in r10] for _ in r9]
 
     # Initializing boxes
-    for n in xrange(9):
-        br = n / 3
-        bc = n % 3
-        for i in xrange(3):
-            for j in xrange(3):
-                row = br * 3 + i
-                col = bc * 3 + j
-                value = sudoku_table[row][col]
-                boxes[n][value] = True
+    for n in r9:
+        br, bc = n / 3, n % 3
+        for i, j in product(r3, r3):
+            row, col = br * 3 + i, bc * 3 + j
+            value = sudoku_table[row][col]
+            boxes[n][value] = True
 
     # Initializing rows and cols
-    for i in xrange(9):
-        for j in xrange(9):
-            value = sudoku_table[i][j]
-            rows[i][j] = True
-            cols[j][i] = True
+    for i, j in product(r9, r9):
+        value = sudoku_table[i][j]
+        rows[i][value] = True
+        cols[j][value] = True
 
     # Initializing possibles
-    for i in xrange(9):
-        for j in xrange(9):
-            if tables[i][j] != 0:
-                for val in xrange(9):
-                    possibles[i][j][val] = False
+    for i, j in product(r9, r9):
+        if tables[i][j] != 0:
+            for val in r10:
+                possibles[i][j].use(val)
             continue
-            bn = 3 * (i / 3) + j / 3
-            for val in xrange(1, 9):
-                if rows[i][val] or cols[j][val] or boxes[bn][val]:
-                    possibles[i][j][val] = False
+        bn = 3 * (i / 3) + j / 3
+        for val in r10[1:]:
+            if rows[i][val] or cols[j][val] or boxes[bn][val]:
+                possibles[i][j].use(val)
 
-    for i in xrange(9):
-        for j in xrange(9):
-            pcounts[i][j] = sum(map(lambda x: 1 if x else 0,
-                                    possibles[i][j][1:]))
-            if pcounts[i][j] != 0:
-                possible_steps.add((i, j))
+    possible_steps = [(i, j) for i, j in
+                      product(r9, r9) if possibles[i][j].count > 0]
+    needs = len([None for i, j in product(r9, r9)
+                 if possibles[i][j].count > 0])
+
+    if needs == 0:
+        if all_solutions:
+            yield sudoku_table
+            raise StopIteration
+        else:
+            return sudoku_table
 
     # Finding the solution
+    table_stack = [(sudoku_table, TableSteps(possible_steps[:]))]
+    was = set([_freeze_table(sudoku_table)])
+    while table_stack:
+        ctable, csteps = table_stack[-1]
+        for cstep in csteps:
 
-    while state_stack:
-        current_state = state_stack.pop()
-        for place in places:
-            for value in xrange(1, 9):
 
